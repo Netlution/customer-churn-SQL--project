79aa5cbe7024f245ad97f1b7b957d0905636b7ae
Netlution <ajagunsamuel2017@gmail.com>
2025-09-21 12:40:05 +0100

    Add new feature

--- Customer_churn.sql
+++ Customer_churn.sql
@@ -111,3 +111,125 @@ WHERE `Total Spend` > (
 SELECT `Subscription Type`, `Contract Length`, AVG(`Total Spend`)
 FROM customer_churn
 GROUP BY `Subscription Type`, `Contract Length`;
+
+
+USE customerchurn;
+
+SELECT * FROM customer_churn;
+
+## Assign row numbers to customers ordered by Total Spend (highest first).
+
+SELECT CustomerID, `Total Spend`,
+       ROW_NUMBER() OVER (ORDER BY `Total Spend` DESC) AS row_num
+FROM customer_churn;
+
+## Rank customers by Total Spend (with ties).
+
+SELECT CustomerID, `Total Spend`,
+       RANK() OVER (ORDER BY `Total Spend` DESC) AS Rank_
+FROM customer_churn;
+
+### Dense rank customers by Total Spend (no gaps).
+SELECT 
+        CustomerID,
+        `Total Spend`,
+        DENSE_RANK() OVER (ORDER BY `Total Spend` DESC) AS denserank
+    FROM customer_churn;
+    
+## Split customers into 4 quartiles based on Total Spend.
+SELECT 
+    CustomerID,
+    `Total Spend`,
+    NTILE(4) OVER (ORDER BY `Total Spend` DESC) AS quartile
+FROM customer_churn;
+
+
+## Compare each customer’s Total Spend with the previous customer.
+SELECT 
+    CustomerID,
+    `Total Spend`,
+    LAG(`Total Spend`, 1) OVER (ORDER BY `Total Spend`) AS prev_spend
+FROM customer_churn;
+
+## Compare each customer’s Total Spend with the next customer.
+SELECT 
+    CustomerID,
+    `Total Spend`,
+    Lead(`Total Spend`, 1) OVER (ORDER BY `Total Spend`) AS prev_spend
+FROM customer_churn;
+
+## Find first and last customer per Subscription_Type.
+
+SELECT 
+    `Subscription Type`,
+    CustomerID,
+    `Total Spend`,
+    FIRST_VALUE(CustomerID) OVER (
+        PARTITION BY `Subscription Type`
+        ORDER BY `Total Spend` ASC
+    ) AS first_customer,
+    LAST_VALUE(CustomerID) OVER (
+        PARTITION BY `Subscription Type` 
+        ORDER BY `Total Spend` ASC
+        ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING
+    ) AS last_customer
+FROM customer_churn;
+
+## Running total of Total Spend by Subscription_Type.
+SELECT 
+    `Subscription Type`,
+    CustomerID,
+    `Total Spend`,
+    SUM(`Total Spend`) OVER (
+        PARTITION BY `Subscription Type`
+        ORDER BY CustomerID
+    ) AS running_total
+FROM customer_churn;
+
+
+## Convert all Subscription_Type values to uppercase.
+SELECT DISTINCT 
+    UPPER(`Subscription Type`) AS subscription_upper
+FROM customer_churn;
+
+## Convert Contract_Length values to lowercase.
+
+SELECT DISTINCT 
+    UPPER(`Contract length`) AS contract_length
+FROM customer_churn;
+
+##Trim spaces from CustomerID (if any).
+
+SELECT CustomerID, TRIM(CustomerID) AS trim_space
+FROM customer_churn;
+
+## Extract first 3 characters of Subscription_Type.
+
+SELECT 
+    `Subscription Type`, 
+    SUBSTRING(`Subscription Type`, 1, 3) AS short_sub
+FROM 
+    customer_churn;
+
+## Combine Gender and Contract_Length into one string.
+SELECT CustomerID, CONCAT(Gender, '-', `Contract Length`) AS gender_contract
+FROM customer_churn;
+ 
+## Replace “Premium” with “Gold” in Subscription_Type.
+
+SELECT REPLACE(`Subscription Type`, 'Premium', 'Gold') as new_type
+FROM customer_churn;
+
+## Find length of each Subscription_Type string.
+
+SELECT `Subscription Type`, LENGTH(`Subscription Type`) as len
+FROM customer_churn;
+
+
+## Add 30 days to Last_Interaction.
+
+SELECT 
+    CustomerID,
+    `Last Interaction`,
+    `Last Interaction` + INTERVAL 30 DAY AS next_check
+FROM customer_churn;
